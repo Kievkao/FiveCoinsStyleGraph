a2cca9ef821b16f891ad1851941523eb05c1b99f
akravchenko <akravchenko@cogniance.com>
2016-04-02 15:20:45 +0300

    Initial Commit

---
+++ FiveCoinsStyleGraph/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FiveCoinsStyleGraph
+//
+//  Created by Andrii Kravchenko on 4/2/16.
+//  Copyright © 2016 kievkao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
